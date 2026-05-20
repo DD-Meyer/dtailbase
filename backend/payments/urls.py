894d1cc926ac72349cfaa5f018a5e36908bec244
DD-Meyer <darynmeyergg@gmail.com>
2026-05-20 16:29:47 +0200

    fix(payments): URL-level csrf_exempt for PayPal endpoints

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,5 +1,6 @@
 # call views in this url file
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import (
     PayPalSubscribeView,
@@ -14,12 +15,12 @@ from .views import (
 urlpatterns = [
     # PayPal Endpoints
     path('plans/', PlansView.as_view(), name='plans'),
-    path('subscribe/', PayPalSubscribeView.as_view(), name='paypal_subscribe'),
-    path('cancel-subscription/', PayPalCancelSubscriptionView.as_view(), name='paypal_cancel_subscription'),
+    path('subscribe/', csrf_exempt(PayPalSubscribeView.as_view()), name='paypal_subscribe'),
+    path('cancel-subscription/', csrf_exempt(PayPalCancelSubscriptionView.as_view()), name='paypal_cancel_subscription'),
     path('billing-summary/', BillingSummaryView.as_view(), name='billing_summary'),
-    path('confirm/', PayPalConfirmView.as_view(), name='paypal_confirm'),
-    path('webhook/', PayPalWebhookView.as_view(), name='paypal_webhook'),
+    path('confirm/', csrf_exempt(PayPalConfirmView.as_view()), name='paypal_confirm'),
+    path('webhook/', csrf_exempt(PayPalWebhookView.as_view()), name='paypal_webhook'),
 
     # Deprecated PayFast endpoint
-    path('payfast-initiate/', PayFastCheckoutView.as_view(), name='payfast_initiate'),
+    path('payfast-initiate/', csrf_exempt(PayFastCheckoutView.as_view()), name='payfast_initiate'),
 ]
